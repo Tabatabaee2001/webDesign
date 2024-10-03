c6a98b4acfb3f8213aea03ca6df3707c44f1881e
Tabatabaee2001 <nargestabatabaee.80@gmail.com>
2024-10-03 15:53:29 +0330

    Add files via upload

---
+++ java/script/jssample.js
@@ -0,0 +1,7 @@
+let fname, lname, fullname, age;
+// variables
+fname='omid';
+lname='lak';
+age=55;
+fullname= fname+ lname;
+console.log(fullname +  ' is:' + age);
---
+++ java/script/script.js
@@ -0,0 +1,8 @@
+let img=document.getElementById("myImage");
+
+function turnon() {
+    img.src='pic_bulbon.gif';
+}
+function turnoff() {
+    img.src='pic_bulboff.gif';
+}
